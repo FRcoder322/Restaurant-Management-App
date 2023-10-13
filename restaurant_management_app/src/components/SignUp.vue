309d52bc45ed621e828c623dfef9ab7fb3ede91a
Florence Rotich <kuravalley@gmail.com>
2023-10-13 19:42:50 +0300

    Restaurant management APP

---
+++ restaurant_management_app/src/components/SignUp.vue
@@ -0,0 +1,47 @@
+<template>
+    <img class="logo" src="../assets/3903248.jpg">
+    <h1>Sign Up</h1>
+
+    <div class ="register">
+        <input type="text" placeholder="Enter Name"/>
+        <input type="text" placeholder="Enter Email"/>
+        <input type="password" placeholder="Enter Password"/>
+        <button>Sign Up</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'SignUp'
+}
+</script>
+<style>
+
+.logo{
+    width:200px;
+    height:200px;
+    border-radius: 50%;
+    overflow: hidden;
+
+}
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid teal;
+    color: white;
+
+}
+.register button{
+    width: 330px;
+    height: 40px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+}
+
+</style>
